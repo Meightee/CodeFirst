344a2d04bd3f85492a04f5ff0fcebc9aa3b6260b
Meightee <anastasiamelnikv@gmail.com>
2020-12-27 22:34:53 +0300

    Добавьте файлы проекта.

---
+++ CodeFirst1/Data/Models/Course.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace CodeFirst.Data.Models
+{
+    public class Course
+    {
+        public Course()
+        {
+            StudentsEnrolled = new HashSet<StudentCourse>();
+            Resources = new HashSet<Resource>();
+            HomeworkSubmissions = new HashSet<Homework>();
+        }
+
+        [Key]
+        public int CourseId { get; set; }
+
+        [Required, MaxLength(64)]
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        [Required]
+        public DateTime StartDate { get; set; }
+
+        [Required]
+        public DateTime EndDate { get; set; }
+
+        [Required]
+        public decimal Price { get; set; }
+
+        public virtual ICollection<StudentCourse> StudentsEnrolled { get; set; }
+
+        public virtual ICollection<Resource> Resources { get; set; }
+
+        public virtual ICollection<Homework> HomeworkSubmissions { get; set; }
+    }
+}
---
+++ CodeFirst1/Program.cs
@@ -0,0 +1,32 @@
+﻿using CodeFirst.Data;
+using CodeFirst.Data.Models;
+using System;
+
+namespace CodeFirst
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var context = new UniversityContext())
+            {
+                var student = new Student();
+                student.Name = "Вася";
+                student.PhoneNumber = "9777120210";
+                student.RegisteredOn = DateTime.Now;
+
+                context.Students.Add(student);
+
+                context.Courses.Add(new Course
+                {
+                    Name = "EF CORE",
+                    Price = 0,
+                    Description = "Курс о разработке прилоложений и тд",
+                    StartDate = DateTime.Today,
+                    EndDate = DateTime.Today.AddDays(30)
+                });
+                context.SaveChanges();
+            }
+        }
+    }
+}
